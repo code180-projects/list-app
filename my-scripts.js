7ffa320e46a0f27259796092eb0c8229060dc2e4
Code180 <42021421+code180-projects@users.noreply.github.com>
2019-02-05 12:31:04 -0700

    Add files via upload

---
+++ my-scripts.js
@@ -0,0 +1,21 @@
+var myList = (JSON.parse(localStorage.getItem("myData")) || [])
+
+function createItem() {
+	var newItem = {text:prompt("Your new item prompt goes here"), style:"none"}
+	myList.push(newItem)
+	localStorage.setItem("myData", JSON.stringify(myList))
+	printItem(newItem)
+}
+
+function printItem(item) {
+	p = document.createElement("p")
+	p.innerHTML = item.text
+	p.className = item.style
+	p.onclick = function () {
+		this.className = item.style = (this.className=="none") ? "strike" : "invisible"
+		localStorage.setItem("myData", JSON.stringify(myList))
+	}
+	document.getElementById("myDiv").appendChild(p)
+}
+
+for (var i=0; i < myList.length; i++) {printItem(myList[i])}
